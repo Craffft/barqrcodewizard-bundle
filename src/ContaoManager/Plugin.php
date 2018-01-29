a43688962af21fe6aab27b815b162d155d12dbf3
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:09:16 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,7 +1,32 @@
 <?php
 
+/*
+ * This file is part of the Barqrcodewizard Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Craffft\BarqrcodewizardBundle\ContaoManager;
 
-class Plugin
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\BarqrcodewizardBundle\CraffftBarqrcodewizardBundle;
+
+class Plugin implements BundlePluginInterface
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftBarqrcodewizardBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['barqrcodewizard']),
+        ];
+    }
 }
